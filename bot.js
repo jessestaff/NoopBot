ba435b53d5ac988c4aa6e933fc822925bcfcf07d
jessestaff <jesstube7779@gmail.com>
2020-04-13 01:03:01 -0400

    first commit

---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const botconfig = require("./botconfig.json")
+
+client.on('ready', () => {
+    console.log(`Logged in!`);
+  });
+
+const prefix = "!";
+
+client.on('message', async (message) => {
+    if (message.content.startsWith(prefix + "pog")) {
+        message.channel.send("POGGERS");
+        message.channel.send("https://tenor.com/view/pogchamp-pog-pogey-poggers-twitch-gif-14340727");
+    }
+
+    if (message.content.startsWith("uwu")) {
+        message.channel.send("shut the fuck up furry")
+    }
+
+    if (message.content.startsWith("owo")) {
+        message.channel.send("shut the fuck up furry")
+    }
+
+    if (message.content.startsWith("the earth is flat")) {
+        message.channel.send("the biggest dumbass in the server goes to <@" + message.author + ">")
+        message.channel.send("good job")
+    }
+
+    if (message.content.startsWith(prefix + "pres")) {
+        message.channel.send("that other bot FUCKING SUCKS DICK and noop is better")
+    }
+})
+
+
+client.login(botconfig.token);
