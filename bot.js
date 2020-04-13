951a1bcbbd037df4095a2576f0c49c9fdfdeac68
jessestaff <jesstube7779@gmail.com>
2020-04-13 18:49:31 -0400

    added comments

--- bot.js
+++ bot.js
@@ -33,39 +33,40 @@ client.on('message', async (message) => {
         message.channel.send("https://tenor.com/view/pogchamp-pog-pogey-poggers-twitch-gif-14340727");
     }
 
+    //uwu command
     if (message.content.startsWith("uwu")) {
         message.channel.send("shut the fuck up furry")
     }
-
+//owo command
     if (message.content.startsWith("owo")) {
         message.channel.send("shut the fuck up furry")
     }
-
+//fake ban command
     if (message.content.startsWith(prefix + "ban ")) {
         message.channel.send("<@" + message.mentions.users.first().id + "> has been banned.")
     }
-
+//gay % command
     if (message.content.startsWith(prefix + "gay ")) {
         message.channel.send("<@" + message.mentions.users.first().id + "> is " + Math.floor(Math.random() * 99) + "% gay.")
     }
-
+//earth flat command
     if (message.content.startsWith("the earth is flat")) {
         message.channel.send("the biggest dumbass in the server goes to <@" + message.author + ">")
         message.channel.send("good job")
     }
-
+//kazak pres command
     if (message.content.startsWith(prefix + "pres")) {
         message.channel.send("kazakhstan president is better :)")
     }
-
+//internet connection command
     if (message.content.startsWith(prefix + "github")) {
         message.channel.send("i am on the internet now B)")
     }
-
+//jessestaff command
     if (message.content.startsWith(prefix + "jessestaff")) {
         message.channel.send("https://tenor.com/view/green-gif-5490562")
     }
-
+//random video command
     if (message.content.startsWith(prefix + "random-vid")) {
         message.channel.send(`here`, {
             files: [
@@ -76,7 +77,7 @@ client.on('message', async (message) => {
 
     
 
-
+//challenger command
     if(message.content.startsWith(prefix + "challenge ")) {
         message.channel.send("<@" + message.author + "> has challenged <@" + message.mentions.users.first().id + ">!")
         const challanged = message.mentions.users.first().id
@@ -89,7 +90,7 @@ client.on('message', async (message) => {
         }
         
     }
-
+//help command
     if (message.content.startsWith(prefix + "help")) {
         const Embedhelp = new Discord.MessageEmbed()
         .setColor('#3C9A0A')
